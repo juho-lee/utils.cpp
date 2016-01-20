2690ccb44a81464167fd546611dac861e6fc0a06
juho-lee <stonecold@postech.ac.kr>
2016-01-20 14:48:14 +0900

    --

--- math.cpp
+++ math.cpp
@@ -34,31 +34,31 @@ double LogGamma(double x)
 // copied from https://github.com/Blei-Lab/lda-c/
 double DiGamma(double x)
 {
-	double p;
-	x = x + 6;
-	p = 1 / (x*x);
-	p = (((0.004166666666667*p - 0.003968253986254)*p +
-		0.008333333333333)*p - 0.083333333333333)*p;
-	p = p + log(x) - 0.5 / x - 1 / (x - 1) - 1 / (x - 2)
-		- 1 / (x - 3) - 1 / (x - 4) - 1 / (x - 5) - 1 / (x - 6);
-	return p;
+    double p;
+    x = x + 6;
+    p = 1 / (x*x);
+    p = (((0.004166666666667*p - 0.003968253986254)*p +
+                0.008333333333333)*p - 0.083333333333333)*p;
+    p = p + log(x) - 0.5 / x - 1 / (x - 1) - 1 / (x - 2)
+        - 1 / (x - 3) - 1 / (x - 4) - 1 / (x - 5) - 1 / (x - 6);
+    return p;
 }
 
 // copied from https://github.com/Blei-Lab/lda-c/
 double TriGamma(double x)
 {
-	double p;
-	int i;
+    double p;
+    int i;
 
-	x = x + 6;
-	p = 1 / (x*x);
-	p = (((((0.075757575757576*p - 0.033333333333333)*p + 0.0238095238095238)
-		*p - 0.033333333333333)*p + 0.166666666666667)*p + 1) / x + 0.5*p;
-	for (i = 0; i < 6; i++) {
-		x = x - 1;
-		p = 1 / (x*x) + p;
-	}
-	return p;
+    x = x + 6;
+    p = 1 / (x*x);
+    p = (((((0.075757575757576*p - 0.033333333333333)*p + 0.0238095238095238)
+                    *p - 0.033333333333333)*p + 0.166666666666667)*p + 1) / x + 0.5*p;
+    for (i = 0; i < 6; i++) {
+        x = x - 1;
+        p = 1 / (x*x) + p;
+    }
+    return p;
 }
 
 double LogSumExp(double a, double b)
